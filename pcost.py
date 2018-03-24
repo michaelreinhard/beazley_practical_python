a30b00752852ea4c2b43158f6a6a38e3d5491b8c
Michael Reinhard <michaelreinhardme.com@Michaels-MacBook-Air.local>
2018-03-24 18:50:56 -0500

    2_7 added try/except, cli, discarded header

--- pcost.py
+++ pcost.py
@@ -1,28 +1,40 @@
 #pcost.py
 
-
-##with open('Data/portfolio.csv', 'rt') as f:
-##    data = f.read()
-##
-##    print(data)
-
 '''Now we turn the script into a function. The
-function gets called at the bottome of the script.'''
+function gets called at the bottome of the script.
+We also add a way to catch bad input data and use a
+library module.
+
+Remember that the first row is a header and has to
+be discarded.'''
+
+import csv
 
 def portfolio_cost(filename):
-    with open('Data/portfolio.csv', 'rt') as f:
-        a = next(f)
+    with open(filename, 'rt') as f:
+##        a = next(f)
+        rows = csv.reader(f)
+        headers = next(rows)
         total = 0
-        for line in f:
-            row = line.split(',')
-            nshares = int(row[1])
-            price = float(row[2])
-            total += nshares*price
+        for row in rows:
+            #split is no longer necessary
+##            row = line.split(','))
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total += nshares*price
+            except ValueError:
+                print("Bad line:", row)
     return total
 
-cost = portfolio_cost('Data/portfolio.csv')
 
-print("Total cost", cost)
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
 
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
     
         
