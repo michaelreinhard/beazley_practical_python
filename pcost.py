75bcca731490be922b6619bf49f2d332ad583332
Michael Reinhard <michaelreinhardme.com@Michaels-MacBook-Air.local>
2018-03-24 17:37:23 -0500

    1_7 change script to a function

--- pcost.py
+++ pcost.py
@@ -6,19 +6,23 @@
 ##
 ##    print(data)
 
-'''A better way to read in a file is to take advantage
-of the fact it has lines.'''
+'''Now we turn the script into a function. The
+function gets called at the bottome of the script.'''
 
-with open('Data/portfolio.csv', 'rt') as f:
-    a = next(f)
-    total = 0
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total += nshares*price
+def portfolio_cost(filename):
+    with open('Data/portfolio.csv', 'rt') as f:
+        a = next(f)
+        total = 0
+        for line in f:
+            row = line.split(',')
+            nshares = int(row[1])
+            price = float(row[2])
+            total += nshares*price
+    return total
 
-print("Total cost", total)
+cost = portfolio_cost('Data/portfolio.csv')
+
+print("Total cost", cost)
 
     
         
