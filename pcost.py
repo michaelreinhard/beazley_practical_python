b44c3ace07bbbc095d40e4709ea1ac011f06dbc5
Michael Reinhard <michaelreinhardme.com@Michaels-MacBook-Air.local>
2018-03-24 19:13:49 -0500

    2_2 cleaning up some of the comments, spacing2_2 cleaning up some of the comments, spacing

--- pcost.py
+++ pcost.py
@@ -1,24 +1,16 @@
 #pcost.py
 
-'''Now we turn the script into a function. The
-function gets called at the bottome of the script.
-We also add a way to catch bad input data and use a
-library module.
-
-Remember that the first row is a header and has to
-be discarded.'''
-
 import csv
 
 def portfolio_cost(filename):
+    '''computes the total cost of a portfolio'''
+
+    total = 0
+    
     with open(filename, 'rt') as f:
-##        a = next(f)
         rows = csv.reader(f)
         headers = next(rows)
-        total = 0
         for row in rows:
-            #split is no longer necessary
-##            row = line.split(','))
             try:
                 nshares = int(row[1])
                 price = float(row[2])
