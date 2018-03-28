ce86756a1aa109abd4ddb39bbda28944b9d05f8a
Michael Reinhard <michaelreinhardme.com@Michaels-MacBook-Air.local>
2018-03-28 18:49:17 -0500

    Added create_table function

--- tableformat.py
+++ tableformat.py
@@ -69,9 +69,19 @@ def create_formatter(name):
     elif name == 'html':
         return HTMLTableFormatter()
     else:
-        raise RuntimeError(f'Unknown table format {name}')
+        raise FormatError(f'Unknown table format {name}')
     
 
+def print_table(objects, columns, formatter):
+    '''
+    Make a nicely formatted table from a list of objects
+    and attributes
+    '''
+    formatter.headings(columns)
+    for obj in objects:
+        rowdata = [str(getattr(obj, name)) for name in columns]
+        formatter.row(rowdata)
+        
     
         
 
