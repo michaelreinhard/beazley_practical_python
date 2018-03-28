334e91e6d6fdabe08c4eefe88381b9e0e4280e9d
Michael Reinhard <michaelreinhardme.com@Michaels-MacBook-Air.local>
2018-03-28 16:56:33 -0500

    4_2 table formatting class with csv, html options

---
+++ tableformat.py
@@ -0,0 +1,78 @@
+#tableformat
+
+class TableFormatter(object):
+    def headings(self, headers):
+        '''
+        Emit the table headings.
+        '''
+        raise notIMplementedError()
+
+    def row(self, rowdata):
+        '''
+        Emit a single row of table data
+        '''
+        raise NotImplementedError()
+
+    
+class TextTableFormatter(TableFormatter):
+    '''
+    Emits a table in plain-text format
+    '''
+    def headings(self, headers):
+        for h in headers:
+            print(f'{h:>10s}', end=' ')
+        print()
+        print(('-'*10 + ' ')*len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f'{d:>10s}', end=' ')
+        print()
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format
+    '''
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    '''
+    Output an HTML formatted table
+    '''
+    def headings(self, headers):
+        print('<tr>', end='')
+        for h in headers:
+            print(f'<th>{h}</th>', end='')
+        print('</tr>')
+
+    def row(self, rowdata):
+        print('<tr>', end='')
+        for d in rowdata:
+            print(f'<td>{d}</td>', end='')
+        print('</tr>')
+    
+
+def create_formatter(name):
+    '''
+    Create an appropriate formatter give output
+    type name.
+    '''
+    if name == 'txt':
+        return TextTableFormatter()
+    elif name == 'csv':
+        return CSVTableFormatter()
+    elif name == 'html':
+        return HTMLTableFormatter()
+    else:
+        raise RuntimeError(f'Unknown table format {name}')
+    
+
+    
+        
+
+    
